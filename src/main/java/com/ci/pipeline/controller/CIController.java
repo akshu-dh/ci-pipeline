e271412a8ad7212810f93ad2750bf5364c271f70
Akshada.MahadevDhavale <akshada.mahadevdhavale@cognizant.com>
2024-02-16 18:01:31 +0530

    initial commit

---
+++ src/main/java/com/ci/pipeline/controller/CIController.java
@@ -0,0 +1,18 @@
+package com.ci.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CIController {
+
+    @GetMapping("/hello")
+    public String greeting(){
+        return "Hello!!!";
+    }
+    @GetMapping("/check")
+    public String check(){
+        return "ci-pipeline checked  ";
+    }
+}
